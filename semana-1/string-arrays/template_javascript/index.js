ede845046a5d2fbd2baf515595d7d86eefa2801f
Mariana Stamatakis <mst.nanasta@gmail.com>
2021-08-11 16:20:00 -0300

    Exercicio-string-array

--- semana-1/string-arrays/template_javascript/index.js
+++ semana-1/string-arrays/template_javascript/index.js
@@ -4,45 +4,45 @@
 
 //Exercícios de escrita
 
-// //Questão 1
+//Questão 1
 
-// const nomeUsuario = prompt("Digite seu nome")
-// const emailUsuario = prompt("Digite seu email")
+const nomeUsuario = prompt("Digite seu nome")
+const emailUsuario = prompt("Digite seu email")
 
-// console.log("O e-mail", emailUsuario," foi cadastrado com sucesso. Seja bem-vinda(o),", nomeUsuario,"!")
+console.log("O e-mail", emailUsuario," foi cadastrado com sucesso. Seja bem-vinda(o),", nomeUsuario,"!")
 
-// //Questão 2
-// const comidasPreferidas = ["sushi","caldo-verde","canjica","cuscus", "feijoada" ]
-// //a.
-// console.log (comidasPreferidas)
-// //b.
-// console.log ("Essas são as minhas comidas preferidas:")
-// console.log (comidasPreferidas[0])
-// console.log (comidasPreferidas[1])
-// console.log (comidasPreferidas[2])
-// console.log (comidasPreferidas[3])
-// console.log (comidasPreferidas[4])
-// //c.
-// let i = 0
-// comidasPreferidas[i+1] = prompt("Qual a sua comida preferida?")
-// console.log (comidasPreferidas)
+//Questão 2
+const comidasPreferidas = ["sushi","caldo-verde","canjica","cuscus", "feijoada" ]
+//a.
+console.log (comidasPreferidas)
+//b.
+console.log ("Essas são as minhas comidas preferidas:")
+console.log (comidasPreferidas[0])
+console.log (comidasPreferidas[1])
+console.log (comidasPreferidas[2])
+console.log (comidasPreferidas[3])
+console.log (comidasPreferidas[4])
+//c.
+let i = 0
+comidasPreferidas[i+1] = prompt("Qual a sua comida preferida?")
+console.log (comidasPreferidas)
 
-// //Questão 3
-// //a.
-// const listaDeTarefas = []
-// //b.
+//Questão 3
+//a.
+const listaDeTarefas = []
+//b.
 
-// listaDeTarefas.push (prompt("Pense em 3 tarefas que precise realizar no dia. Agora digite a 1ª tarefa"))
-// listaDeTarefas.push (prompt("Agora digite a 2ª tarefa"))
-// listaDeTarefas.push (prompt("Agora digite a 3ª tarefa"))
-// //c.
-// console.log (listaDeTarefas)
-// //d.
-// let indiceEscolhido = prompt("Qual dessas tarefas você já realizou? Responda com 0, 1 ou 2")
-// //e.
-// listaDeTarefas.splice(indiceEscolhido, 1)
-// //f.
-// console.log (listaDeTarefas)
+listaDeTarefas.push (prompt("Pense em 3 tarefas que precise realizar no dia. Agora digite a 1ª tarefa"))
+listaDeTarefas.push (prompt("Agora digite a 2ª tarefa"))
+listaDeTarefas.push (prompt("Agora digite a 3ª tarefa"))
+//c.
+console.log (listaDeTarefas)
+//d.
+let indiceEscolhido = prompt("Qual dessas tarefas você já realizou? Responda com 0, 1 ou 2")
+//e.
+listaDeTarefas.splice(indiceEscolhido, 1)
+//f.
+console.log (listaDeTarefas)
 
 //Desafios
 //Questão 1
